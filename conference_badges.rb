2ff1c39244d226166e83ca4a71f460957347fbbd
Chance cline <clinechance@gmail.com>
2020-01-30 02:57:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(attendees)
+    attendees.map do |attendees|
+  end
+end
