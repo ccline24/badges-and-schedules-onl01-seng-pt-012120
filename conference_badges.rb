350b144d81bd98a5a8d78e7b926fa70bf406203a
Chance cline <clinechance@gmail.com>
2020-01-30 03:12:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendees, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}"
+    "Hello, #{attendees}! You'll be assigned to room #{index+1}"
   end
 end
